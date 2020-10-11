da8a57d51ed8df1557919bcd9382865d9a211c33
yangt <Icui4cu6>
2020-10-11 15:24:44 +0800

    C语言代码测试 Initial commit

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    printf("int 存储大小:%lu \n",sizeof(int ));//%lu 为 32 位无符号整数 详细说明：https://www.runoob.com/cprogramming/c-function-printf.html
+
+
+    return 0;
+}
